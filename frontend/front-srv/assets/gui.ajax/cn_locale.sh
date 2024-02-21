884bb956977283dca539027ff89d24bb6e0b1427
chaos <chaos@veigit.com>
2024-02-21 10:30:05 +0800

    add cn locale conversion scripts for dayjs calendar plugin

---
+++ frontend/front-srv/assets/gui.ajax/cn_locale.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i 's/Yesterday at/昨天/g' res/dist/libs/PydioReactUI.min.js
+sed -i 's/Tomorrow at/明天/g' res/dist/libs/PydioReactUI.min.js
+sed -i 's/Today at/今天/g' res/dist/libs/PydioReactUI.min.js
